0d8916f3b95ce0696126ad3f24902e602f334905
Linares <akuruma025@yandex.ru>
2025-03-28 23:53:13 +0300

    добавление заготовок для обработки входа/выхода/регистрации HR

---
+++ server/hr_client/user.go
@@ -0,0 +1,15 @@
+package hrclient
+
+import ()
+
+func Login(username string, password string) string {
+	return ""
+}
+
+func Logout(username string) string {
+	return ""
+}
+
+func Register(username string, password string, email string) string {
+	return ""
+}
