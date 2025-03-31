949889b85acd880bfeb951f34523a60295c14ce8
Linares <akuruma025@yandex.ru>
2025-03-31 15:51:31 +0300

    исправление миграций

--- server/db/000001_create_project_struct.up.sql
+++ server/db/000001_create_project_struct.up.sql
@@ -1,34 +1,34 @@
-CREATE TABLE IF NOT EXISTS "hr" (
+CREATE TABLE "hr" (
   "id" serial PRIMARY KEY,
   "email" text UNIQUE,
   "hash_password" text,
   "username" text UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "vacancies" (
+CREATE TABLE "vacancies" (
   "id" serial PRIMARY KEY,
   "name" text
 );
 
-CREATE TABLE IF NOT EXISTS "vacantion_hard_skills" (
+CREATE TABLE "vacantion_hard_skills" (
   "id" serial PRIMARY KEY,
   "vacancy_id" integer,
   "hard_skill_id" integer
 );
 
-CREATE TABLE IF NOT EXISTS "vacantion_soft_skills" (
+CREATE TABLE "vacantion_soft_skills" (
   "id" serial PRIMARY KEY,
   "vacancy_id" integer,
   "soft_skill_id" integer
 );
 
-CREATE TABLE IF NOT EXISTS "finders" (
+CREATE TABLE "finders" (
   "id" serial PRIMARY KEY,
   "portfolio" boolean,
   "hr_id" integer
 );
 
-CREATE TABLE IF NOT EXISTS "resumes" (
+CREATE TABLE "resumes" (
   "id" serial PRIMARY KEY,
   "first_name" text,
   "last_name" text,
@@ -39,35 +39,47 @@ CREATE TABLE IF NOT EXISTS "resumes" (
   "finder_id" integer
 );
 
-CREATE TABLE IF NOT EXISTS "portfolio" (
+CREATE TABLE "resume_hard_skill" (
+  "id" serial PRIMARY KEY,
+  "resume_id" integer,
+  "hard_skill_id" integer
+);
+
+CREATE TABLE "resume_soft_skill" (
+  "id" serial PRIMARY KEY,
+  "resume_id" integer,
+  "soft_skill_id" integer
+);
+
+CREATE TABLE "portfolio" (
   "id" serial PRIMARY KEY,
   "finder_id" integer
 );
 
-CREATE TABLE IF NOT EXISTS "portfolio_hard_skills" (
+CREATE TABLE "portfolio_hard_skills" (
   "id" serial PRIMARY KEY,
   "portfolio_id" integer,
   "hard_skill_id" integer
 );
 
-CREATE TABLE IF NOT EXISTS "portfolio_soft_skills" (
+CREATE TABLE "portfolio_soft_skills" (
   "id" serial PRIMARY KEY,
   "portfolio_id" integer,
   "soft_skill_id" integer
 );
 
-CREATE TABLE IF NOT EXISTS "portfolio_links" (
+CREATE TABLE "portfolio_links" (
   "id" serial PRIMARY KEY,
   "link" text,
   "portfolio_id" integer
 );
 
-CREATE TABLE IF NOT EXISTS "hard_skills" (
+CREATE TABLE "hard_skills" (
   "id" serial PRIMARY KEY,
   "hard_skill" text
 );
 
-CREATE TABLE IF NOT EXISTS "soft_skills" (
+CREATE TABLE "soft_skills" (
   "id" serial PRIMARY KEY,
   "soft_skill" text
 );
@@ -97,3 +109,11 @@ ALTER TABLE "portfolio_soft_skills" ADD FOREIGN KEY ("soft_skill_id") REFERENCES
 ALTER TABLE "portfolio_hard_skills" ADD FOREIGN KEY ("hard_skill_id") REFERENCES "hard_skills" ("id");
 
 ALTER TABLE "portfolio_links" ADD FOREIGN KEY ("portfolio_id") REFERENCES "portfolio" ("id");
+
+ALTER TABLE "resume_soft_skill" ADD FOREIGN KEY ("resume_id") REFERENCES "resumes" ("id");
+
+ALTER TABLE "resume_hard_skill" ADD FOREIGN KEY ("resume_id") REFERENCES "resumes" ("id");
+
+ALTER TABLE "resume_hard_skill" ADD FOREIGN KEY ("hard_skill_id") REFERENCES "hard_skills" ("id");
+
+ALTER TABLE "resume_soft_skill" ADD FOREIGN KEY ("soft_skill_id") REFERENCES "soft_skills" ("id");
